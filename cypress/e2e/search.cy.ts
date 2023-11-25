01cfe461dd068c3cac3136c2b22296dd8ee49bb6
vyk <vykmnsk@gmail.com>
2023-11-25 21:22:53 +1100

    Enhance text verifications

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -8,8 +8,17 @@ describe('Search', () => {
 
     cy.get('#search').type(`${criteria}{enter}`);
     cy.url().should('include', '/catalogsearch/result/?q=');
+
     const expectedTitle = `Search results for: '${criteria}'`;
-    cy.get("h1").should('include.text', expectedTitle);
+    const normText = (t) => t.trim().toLocaleLowerCase();
+
+    cy.title().should(($text) => {
+      expect(normText($text)).to.equal(normText(expectedTitle));
+    })
+
+    cy.get("h1").should(($div) => {
+      expect(normText($div.text())).to.equal(normText(expectedTitle));
+    })
 
     cy.get('div.product-item-info').should('have.length.above', 0);
   })
