3ddc9b8387a483814f9204c3c415a1be8e303ab1
vyk <vykmnsk@gmail.com>
2023-11-26 22:18:13 +1100

    Add custom cmd for saved search

--- cypress/e2e/search-results.cy.ts
+++ cypress/e2e/search-results.cy.ts
@@ -1,15 +1,13 @@
 import { page } from "../support/page-elements";
 
+
 describe('Search Results', () => {
-  beforeEach(() => {
-    const query = 'pants';
-    cy.visitUrl(`/catalogsearch/result/?q=${query}`)
-  })
 
   it('updates page titles with search query', () => {
-    const criteria = 'pants';
+    const query = 'something';
+    cy.savedSearchFor(query);
 
-    const expectedTitle = `Search results for: '${criteria}'`;
+    const expectedTitle = `Search results for: '${query}'`;
     const normText = (t: string) => t.trim().toLocaleLowerCase();
 
     cy.title().should(($text) => {
@@ -19,11 +17,16 @@ describe('Search Results', () => {
     cy.get(page.title).should(($div) => {
       expect(normText($div.text())).to.equal(normText(expectedTitle));
     })
-
   })
 
   it('finds existing products', () => {
+    cy.savedSearchFor('pants');
     cy.verifyProducts();
+  })
+
+  it('shows no results for non-existing products', () => {
+    cy.savedSearchFor('dummy');
+    cy.contains('Your search returned no results');
   })
 
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,51 +2,16 @@
 
 import { page } from "./page-elements";
 
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
-
 
 const BASE_URL = 'https://magento.softwaretestingboard.com';
+const SEARCH_RESULT_URL = '/catalogsearch/result';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable<Subject> {
-      visitUrl(pageName: string): void
+      visitUrl(pageName: string): void,
+      savedSearchFor(query: string): void,
       verifyProducts(): Chainable<Element>
     }
   }
@@ -56,16 +21,18 @@ Cypress.Commands.add('visitUrl', (pageName) => {
   cy.visit(`${BASE_URL}/${pageName}`)
 });
 
+Cypress.Commands.add('savedSearchFor', (query) => {
+  cy.visitUrl(`${SEARCH_RESULT_URL}/?q=${query}`)
+});
+
 Cypress.Commands.add('verifyProducts', () => {
   cy.get(page.products).then(($products) => {
     expect($products).to.have.length.at.least(1);
     $products.each(($_, $prod) => {
       cy.wrap($prod).find(page.product.name).should('have.length', 1);
-      // cy.wrap($prod).find(page.product.price).should('have.length', 1);
     })
   });
 });
-
 
 export { }
 
